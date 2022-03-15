4b3369ddd2caf1503fc26d9a9895b56e92601747
A1093321 <100329367+A1093321@users.noreply.github.com>
2022-03-15 23:53:38 +0800

    Add files via upload

---
+++ 墾丁三日遊表單.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+
+<link rel='icon' href='/nuk.bmp' type='image/x-icon' />
+<title>墾丁三日遊</title>
+
+</head>
+
+<body bgcolor='#EEFFA6'> 
+
+<h1><b><CENTER><FONT COLOR='red'>墾丁三日遊</FONT></CENTER></b></h1>
+<hr color='#A8FFE8' width='500'>
+<CENTER><a href='https://www.nuk.edu.tw/'><img src='/nuk.bmp' width='13%'></a>
+<a href='https://www.pthg.gov.tw/Content_List.aspx?n=CB677664C5CE6E8A/'><img src='/K.jpg' width='20%'></a></CENTER>
+<br/><CENTER>Please input your Name: <input type='text' placeholder='name'></CENTER><br/><br/>
+<CENTER>Please input your ID: <input type='password' placeholder='A10933XX'></CENTER><br/><br/>
+<CENTER>Please input your Email: <input type='email' placeholder='email'></CENTER><br/><br/>
+<CENTER>Please input your PhoneNumber: <input type='text' placeholder='09XX-XXX-XXX'></CENTER><br/><br/>
+<CENTER>Please input your PassWord: <input type='text' name='PWD' placeholder='password'></CENTER><br/><br/>
+<CENTER>Please input your birthday: <input type='date'></CENTER><br/><br/>
+<CENTER>Please choose your gender: <input type='radio'>男<input type='radio'>女<input type='radio'>其他</CENTER><br/><br/>
+<CENTER>Please choose your T-shirt size: <select name='size'><option>S</option><option>M</option><option>L</option><option>XL</option></select></CENTER><br/><br/>
+<CENTER>Please choose your favorite color: <input type='color'></CENTER><br/><br/>
+<CENTER>Please choose your food preference: <input type='checkbox'>肉類<input type='checkbox'>蔬菜<input type='checkbox'>甜點<input type='checkbox'>炸物</CENTER><br/><br/>
+<CENTER>Please input tickets you need: <input type='number'></CENTER><br/><br/>
+<CENTER>Please choose the city you prefer: <select name='city'><option>高雄</option><option>台東</option></select></CENTER><br/><br/>
+
+<CENTER><input type='submit'></CENTER>
+
+<h1><b><CENTER><FONT COLOR='red'>活動使用</FONT></CENTER></b></h1>
+<hr color='#A8FFE8' width='500'>
+<br/><CENTER>Please input the Time now: <input type='time'></CENTER><br/><br/>
+<CENTER>How you prefer to dance: <input type='range'></CENTER><br/><br/>
+<CENTER>How you prefer to play baseball: <input type='range'></CENTER><br/><br/>
+<CENTER>How you prefer to play basketball: <input type='range'></CENTER><br/><br/>
+<CENTER>How you prefer to play football: <input type='range'></CENTER><br/><br/>
+<CENTER>How you prefer to play volleyball: <input type='range'></CENTER><br/><br/>
+<CENTER>How you prefer to play badminton: <input type='range'></CENTER><br/><br/>
+<CENTER>How you prefer to swim: <input type='range'></CENTER><br/><br/>
+
+<CENTER><input type='submit'></CENTER><br/><br/><br/><br/>
+
+<h3><CENTER>衣服尺碼對照表</CENTER></h3>
+
+<CENTER><table border=10>
+<TR><TH>null</TH><TH>S</TH><TH>M</TH><TH>L</TH><TH>XL</TH></TR>
+<TR><TH>肩寬</TD><TD>37</TD><TD>38</TD><TD>39</TD><TD>40</TD></TR>
+<TR><TH>胸圍</TD><TD>80</TD><TD>85</TD><TD>90</TD><TD>95</TD></TR>
+<TR><TH>腰圍</TD><TD>64</TD><TD>68</TD><TD>72</TD><TD>76</TD></TR>
+</table></CENTER>
+
+
+</body>
+
+</html>
